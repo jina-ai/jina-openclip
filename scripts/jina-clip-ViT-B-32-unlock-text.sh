96aee42b4376deab79804e8ed024166d5238852f
George Mastrapas <georgios.mastrapas@jina.ai>
2024-02-29 20:31:21 +0200

    fix: LLRD run scripts

--- scripts/jina-clip-ViT-B-32-unlock-text.sh
+++ scripts/jina-clip-ViT-B-32-unlock-text.sh
@@ -2,13 +2,14 @@ export CUDA_VISIBLE_DEVICES=1
 
 python -m training.main \
     --train-data="/shared/datasets/laion-400m/laion400m-data-new/{00000..01720}.tar::/shared/datasets/laion-400m/laion400m-data/{00000..01200}.tar" \
-    --train-num-samples 120000 \
+    --train-num-samples 1572864 \
     --val-data="/shared/datasets/laion-400m/laion400m-data-new/{01721..01722}.tar" \
     --val-num-samples 12000 \
     --dataset-type webdataset \
     --batch-size 1024 \
-    --warmup 1000 \
-    --epochs 10 \
+    --grad-checkpointing \
+    --warmup 3072 \
+    --epochs 50 \
     --lr 5e-4 \
     --text-lr-decay 1.0 \
     --vision-lr-decay 1.0 \
@@ -19,7 +20,7 @@ python -m training.main \
     --log-every-n-steps 20 \
     --report-to "wandb" \
     --name "jina-clip-ViT-B-32-unlock-text" \
-    --wandb-project-name "jina-clip-laion400m-llrd" \
+    --wandb-project-name "jina-clip-laion400m-short-LLRD" \
     --clip-benchmark-frequency 1 \
     --mteb-frequency 1 \
     --evaluate-on-start
