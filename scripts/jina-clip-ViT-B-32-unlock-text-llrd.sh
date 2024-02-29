df42c53a4e54aa07f3a074b6a8728657cbfa0e4b
George Mastrapas <georgios.mastrapas@jina.ai>
2024-02-29 18:46:20 +0200

    feat: add LLRD test scripts

---
+++ scripts/jina-clip-ViT-B-32-unlock-text-llrd.sh
@@ -0,0 +1,25 @@
+export CUDA_VISIBLE_DEVICES=2
+
+python -m training.main \
+    --train-data="/shared/datasets/laion-400m/laion400m-data-new/{00000..01720}.tar::/shared/datasets/laion-400m/laion400m-data/{00000..01200}.tar"
+    --train-num-samples 120000
+    --val-data="/shared/datasets/laion-400m/laion400m-data-new/{01721..01722}.tar"
+    --val-num-samples 12000 \
+    --dataset-type webdataset \
+    --batch-size 1024 \
+    --warmup 1000 \
+    --epochs 10 \
+    --lr 5e-4 \
+    --text-lr-decay 0.98 \
+    --vision-lr-decay 0.98 \
+    --precision amp \
+    --workers 4 \
+    --model "jina-clip-ViT-B-32" \
+    --force-custom-text \
+    --log-every-n-steps 20 \
+    --report-to "wandb" \
+    --name "jina-clip-ViT-B-32-unlock-text-llrd" \
+    --wandb-project-name "jina-clip-laion400m-llrd" \
+    --clip-benchmark-frequency 1 \
+    --mteb-frequency 1 \
+    --evaluate-on-start
