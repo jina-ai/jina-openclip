af9ef19adcfa4d0029f80fc85e728aaef7552bc7
George Mastrapas <georgios.mastrapas@jina.ai>
2024-03-21 15:56:29 +0200

    fix: add fp16 checkpoint

--- src/open_clip/pretrained.py
+++ src/open_clip/pretrained.py
@@ -545,9 +545,7 @@ _PRETRAINED = {
     ),
     'jina-clip-ViT-B-16-dino': dict(
         jina_clip=_pcfg(hf_hub='jinaai/jina-backbone-dino-ViT-B-16/'),
-    ),
-    'jina-clip-ViT-B-16-dino-fp16': dict(
-        jina_clip=_pcfg(hf_hub='jinaai/jina-backbone-dino-ViT-B-16-fp16/'),
+        jina_clip_fp16=_pcfg(hf_hub='jinaai/jina-backbone-dino-ViT-B-16-fp16/'),
     ),
 }
 
